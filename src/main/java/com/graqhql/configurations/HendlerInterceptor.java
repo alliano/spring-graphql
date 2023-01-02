c8660fc6c809511938b8813f20de1860696d33de
alliano <allianoanonymous@gmail.com>
2023-01-02 19:51:18 +0900

    initial commit

--- src/main/java/com/graqhql/configurations/HendlerInterceptor.java
+++ src/main/java/com/graqhql/configurations/HendlerInterceptor.java
@@ -14,16 +14,16 @@ import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
 @Configuration
-public class HeaderInterCeptor implements WebGraphQlInterceptor {
+public class HendlerInterceptor implements WebGraphQlInterceptor {
 
     @Override
     public Mono<WebGraphQlResponse> intercept(WebGraphQlRequest request, Chain chain) {
-        HttpHeaders heders = request.getHeaders();
-        Map<String, List<String>> datas = new HashMap<>(heders);
-        request.configureExecutionInput(
-            (executionInput, builder) ->
-            builder.graphQLContext(
-                Collections.singletonMap("headers", datas)).build());
+        HttpHeaders headers = request.getHeaders();
+        Map<String, List<String>> datas = new HashMap<>(headers);
+        request.configureExecutionInput( (executionInput, builder) ->
+        builder.graphQLContext(
+            Collections.singletonMap("headers", datas)).build()
+        );
         return chain.next(request);
     }
 
